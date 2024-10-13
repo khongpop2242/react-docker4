f2badfaea7f5795c725dc030387c636d3ccc6aed
Suwattt <pond12485@gmail.com>
2024-10-13 14:48:28 +0700

    edit Mycard and workflow file

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -26,6 +26,7 @@ export default function MyCard(props) {
     </Typography>
   </CardContent>
   <CardActions>
+    <Button size="small">XXX.COM</Button>
     <Button size="small">Learn More</Button>
   </CardActions>
 </Card>
